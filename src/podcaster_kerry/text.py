01f61ce4732226dee5ce6527f81aeb5f9275e0be
Kerry Wang <43691721+WorldofKerry@users.noreply.github.com>
2025-03-11 13:02:18 -0700

    use pdftotext

--- src/podcaster_kerry/text.py
+++ src/podcaster_kerry/text.py
@@ -1,15 +1,6 @@
 import os
-import logging
-from tika import parser
 
-# Silence tika logs https://github.com/chrismattmann/tika-python/issues/341
-tika_logger = logging.getLogger("tika.tika")
-tika_logger.setLevel(logging.WARNING)
-
-def extract_text(path: os.PathLike) -> str:
-    while True:
-        try:
-            parsed = parser.from_file(str(path))
-        except RuntimeError as e:
-            print(f"Error with extract_text {e}, retrying ...")
-    return parsed["content"]
+def extract_text(in_path: os.PathLike):
+    output = subprocess.check_output(["pdftotext", str(in_path), "-"], stderr=subprocess.STDOUT)
+    text = output.decode("utf-8")
+    return text
